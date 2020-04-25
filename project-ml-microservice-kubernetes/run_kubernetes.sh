91ad938a990e2f67c56c43fa858d6d0548995986
pm17 <parveshmourya96@gmail.com>
2020-04-25 19:56:26 +0530

    final check

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -9,7 +9,7 @@ dockerpath=parveshmourya/udacity-microservice-p4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run udacity-microserive-p4-dev --image=parveshmourya/udacity-microservice-p4 --port=80
+kubectl run udacity-microservice-p4 --image=parveshmourya/udacity-microservice-p4 --port=80
 
 
 # Step 3:
@@ -18,4 +18,4 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl expose deployment udacity-microserive-p4-dev --port=8000 --target-port=80
+kubectl port-forward pod/udacity-microservice-p4 8000:80
