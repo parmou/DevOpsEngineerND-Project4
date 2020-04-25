a816e820dbcee1ac95f114926de4d3153669dba4
pm17 <parveshmourya96@gmail.com>
2020-04-25 16:35:17 +0530

    Task5: complete

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=app_make_prediction .
+docker build --tag=parveshmourya/udacity-microservice-p4 .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker images ls
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 app_make_prediction
+docker run -p 8000:80 parveshmourya/udacity-microservice-p4
--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=parveshmourya/udacity-microservice-p4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run udacity-microserive-p4-dev --image=parveshmourya/udacity-microservice-p4 --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl expose deployment udacity-microserive-p4-dev --port=8000 --target-port=80
